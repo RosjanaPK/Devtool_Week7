3eda06fdb02e85d0d99306a53bf197c1b799afd1
RosjanaPK <rajna471@gmail.com>
2023-03-04 01:46:22 +0700

    update

--- myscript.py
+++ myscript.py
@@ -1,11 +1,12 @@
 import argparse
 
+
 parser = argparse.ArgumentParser()
-parser.add_argument("name", help="the name to greet")
+parser.add_argument("NAME", help="the name to greet")
 parser.add_argument("--age", help="the age of the person being greeted", type=int)
 args = parser.parse_args()
 
-greeting = "Hello, " + args.name + "!"
+greeting = "Hello, " + args.NAME + "!"
 if args.age:
     greeting += " You are " + str(args.age) + " years old."
 
